c8e50d8a7144a9c88ca7f57474546fc40ce6f283
matthias haefeli <haefeli.matthias@gmail.com>
2020-09-19 14:22:07 -0600

    add axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Navbar from './components/Navbar/Navbar';
+import Message from './components/Message/Message'
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Navbar />
+        <Message />
       </React.Fragment>
     )
   }
